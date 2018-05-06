b94b8390fa74f0780298fc7bddb0c476c965b8e3
miaolegemi <313457089@qq.com>
2018-05-06 23:28:37 +0800

    '乱七八糟'

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 //app.js
+
+//app.js
+const AV = require('./libs/av')
+const wafer = require('./vendors/wafer-client-sdk/index')
+// 引入 leanCloud 支持文件储存 https://leancloud.cn/docs/weapp.html#hash799103847
+AV.init({
+  appId: 'wjwHIjxzCiO2c1QO50HMevCh-gzGzoHsz',
+  appKey: 'M3fajs4ucerUF62BKyGUXInj',
+})
+
+
 App({
+  config: {
+    host: 'group.mrourou.com'
+  },
   onLaunch: function () {
     var token = wx.getStorageSync('token')
     var that = this
