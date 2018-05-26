05a1c24b469c6e5c4b7b1e4d6105ca342765b33b
wangmeng <wangmeng27@58ganji.com>
2018-05-26 20:29:39 +0800

    '礼物支付'

--- components/gift-cont.js
+++ components/gift-cont.js
@@ -1,15 +1,26 @@
 // components/gift-cont.js
+/**
+*礼物弹窗组件 
+*properties 接受父组件传来的roomId showGift(用于触发动画 TODO)
+* triggerEvent close（父组件监听并关闭弹窗） sendGift（把支付订单信息传给父组件，并触发弹幕功能....因为发送信息时间在父组件）
+**/ 
 const request = require('../utils/request')
 Component({
   /**
    * 组件的属性列表
+   * 接受父组件传值
    */
   properties: {
     roomId: {
-      type: String, 
-      value: '', 
-      observer: function(newVal, oldVal){} 
-      }
+      type: String,
+      value: '',
+      observer: function (newVal, oldVal) { }
+    },
+    showGift: {
+      type: Boolean,
+      value: false,
+      observer: function (newVal, oldVal) { }
+    }
   },
 
   /**
@@ -17,27 +28,62 @@ Component({
    */
   data: {
     num: 1,
-    wish:'恭喜恭喜~',
-    activeIndex:0,
+    wish: '恭喜恭喜~',
+    activeIndex: 0,
     giftArr: [{
-      imgURL: '../res/003.png',
-      name: '打飞机',
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/qixi.png',
+      name: '祝福',
       giftId: 1,
-      price:'0.01'
+      price: '0.01'
     }, {
-      imgURL: '../res/003.png',
-      name: '小飞机',
-      giftId: 2,      
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/hug.png',
+      name: '抱一抱',
+      giftId: 2,
       price: '0.1'
-      }, {
-        imgURL: '../res/003.png',
-        name: '打飞机',
-        giftId: 3,
-        price: '1.00'
     }, {
-      imgURL: '../res/003.png',
-      name: '打飞机',
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/rose.png',
+      name: '红玫瑰',
+      giftId: 3,
+      price: '1.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/mmd.png',
+      name: '么么哒',
       giftId: 4,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/car.png',
+      name: '跑车',
+      giftId: 5,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/like.png',
+      name: '爱心',
+      giftId: 6,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/star.png',
+      name: '星星',
+      giftId: 7,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/666.png',
+      name: '666',
+      giftId: 8,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/ams.png',
+      name: '爱马仕',
+      giftId: 9,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/jiezhi.png',
+      name: '钻戒',
+      giftId: 10,
+      price: '10.00'
+    }, {
+      imgURL: 'http://msh.mrourou.com/addons/meepo_xianchang/template/mobile/app/images/baping/images/gift/lafei.png',
+      name: '拉菲',
+      giftId: 11,
       price: '10.00'
     }],
     // 默认应该是第一个的单价
@@ -46,7 +92,7 @@ Component({
     // roomId:-1,
   },
   attached() {
-    console.log(this.data.roomId)
+    console.log(this.data)
   },
   /**
    * 组件的方法列表
@@ -72,11 +118,11 @@ Component({
       })
     },
     minus() {
-      if (this.data.num ==1) {
-        return 
+      if (this.data.num <= 1) {
+        return
       }
       this.setData({
-          num: + this.data.num - 1
+        num: + this.data.num - 1
       });
     },
     plus() {
@@ -92,47 +138,59 @@ Component({
     },
     pay() {
       // 请求订单签名
+      if (this.data.num <= 0) {
+        wx.showToast({
+          title: '亲数量要大于1哦~',
+          icon: 'success',
+          // image:'/../res/003.png',
+          duration: 2000
+        })
+        return 
+      }
       this.getPayOrderInfo()
     },
     getPayOrderInfo() {
-      let total = this.data.unitPrice * this.data.num  
+      let total = this.data.unitPrice * this.data.num
       let that = this
       request({
-        url:'/api/wx/pay/unified/order',
-        data:{
-            // 'giftId': this.data.giftId,
-            // 'number': total,
-            //  'roomId': this.data.roomId 
+        url: '/api/wx/pay/unified/order',
+        data: {
+          // 'giftId': this.data.giftId,
+          // 'number': this.data.num,
+          //  'roomId': this.data.roomId 
           'giftId': 1,
           'number': 1,
           'roomId': 1
         },
         success(res) {
           let data = res.data
-            if (data.code ===  200) {
-              that.payAll(data.data)
-            } else {
-              wx.showToast({
-                title: '支付失败',
-                icon: 'success',
-                duration: 2000
-              })
-            }
+          if (data.code === 200) {
+            that.payAll(data.data)
+          } else {
+            wx.showToast({
+              title: '支付失败',
+              icon: 'success',
+              duration: 2000
+            })
+          }
         },
       })
     },
     payAll(val) {
+      let that = this
       wx.requestPayment({
         'timeStamp': val.timeStamp,
         'nonceStr': val.nonceStr,
         'package': val.ppackage,
-        'signType': val.timeStamp,
+        'signType': val.signType,
         'paySign': val.paySign,
         'success': function (res) {
-          console.log(res)
+          // 触发父组件事件
+          let myEventDetail = {a:1} // detail对象，提供给事件监听函数
+          let myEventOption = {} // 触发事件的选项
+          that.triggerEvent('sendGift', myEventDetail, myEventOption)
         },
         'fail': function (res) {
-          console.log(res)
           wx.showToast({
             title: '支付失败',
             icon: 'success',
--- pages/chat/index.js
+++ pages/chat/index.js
@@ -4,258 +4,245 @@ const app = getApp()
 const request = require('../../utils/request')
 
 Page({
-    data: {
-        animate: true,
-        placeholderText: "连接服务器中...",
-        messageArray: [
-        //   {
-        //     id:'0',
-        //     type: 'self',
-        //     name: 'wang',
-        //     time: '2000-2-2',
-        //     message: 'zhongjiashigedashabi'
-        // }
-        ],
-        socketOpen: false,
-        inputValue: "",
-        userInfo: {},
-        roomId: -1,
-        isLogin: false, //判断是否登录，显示/隐藏登录btn
-        focusHeight: '8px',
-        showGift: false, //显示礼物组件
-    },
-    onLoad: function (options) {
-        this.data.roomId = options.roomId || 1;
-        if (app.globalData.token) {
-            this.checkUserInfo()
-            this.initStompClient()
-        } else {
-            app.loginCallback = res => {
-                this.checkUserInfo()
-                this.initStompClient()
-            }
-        }
-    },
-    onUnload: function () {
-        // 关闭socket
-        wx.closeSocket();
-    },
-    bindKeyInput: function (e) {
-        this.setData({
-            inputValue: e.detail.value
-        });
-    },
-    // 连接websocket聊天 接受广播信息
-    initStompClient: function () {
-        const self = this;
-        client.init(() => {
-            this.setData({
-                socketOpen:true,
-                placeholderText: "连接服务器成功，请输入信息。"
-            })
-        });
-        client.connect({'wx-group-token':app.globalData.token}, function (sessionId) {
-            client.subscribe(chatConfig.subcribeUrl+'/'+self.data.roomId, function (body, headers) {
-                var data = JSON.parse(body.body).data;
-                console.log('接收');
-                console.log(data);
-                // this.handleMessage(data.message)
-                var newMessage = {
-                    id: data.message.id,
-                    type: 'other',
-                    isText: data.message.type === 'TEXT' ? true : false, 
-                    name: data.user.nikename,
-                    time: 'message.sendTime',
-                    avatarUrl: data.user.avatarurl,
-                    message:  data.message.detail 
-                };
-                newMessage.type = data.user.openid === self.data.userInfo.openId ? 'self' : 'other';
-                var newArray = [...self.data.messageArray,newMessage];
-                self.setData({
-                    messageArray: newArray,
-                    placeholderText: "请输入信息",
-                    num: newArray[newArray.length-1].id
-                });
-            });
-        })
-    },
-    send: function () {
-        if (this.data.inputValue !== "") {
-          this.sendSocketMessage({ type: 'TEXT', detail: this.data.inputValue });
-          this.setData({
-            inputValue: ""
-          });
-        }
-    },
-    // 发送websocket信息
-    // @param {
-    //    type: '',
-    //    detail:
-    // }
-    sendSocketMessage: function (obj) {
-        var self = this;
-        if (self.data.socketOpen) {
-            client.send(chatConfig.sendMsgUrl+'/'+self.data.roomId,
-                { priority: 9 },
-                JSON.stringify(obj))
-        }
-    },
-    checkUserInfo: function (e) {
-        wx.getUserInfo({
-            // 带上登录信息
-            withCredentials: true,
-            success: res => {
-                // 成功获取用户信息
-                this.sendEncryptedData(res.encryptedData, res.iv)
-            },
-            fail: res => {
-                console.log(res)
-                // 显示登录按钮，引导登录
-                this.setData({
-                  isLogin: false,
-                })
-            }
-        })
-    },
-    // 手动授权登录
-    updateUserInfo: function(res) {
-        console.log(res)
-        this.sendEncryptedData(res.detail.encryptedData, res.detail.iv)
-    },
-    // 获取用户信息
-    sendEncryptedData: function (encryptedData, iv) {
-        let self = this
-        request({
-            url: '/api/wx/user',
-            data:{
-                encryptedData: encryptedData,
-                iv: iv
-            },
-            success: function (res) {
-                console.log(res)
-                if (res.data.code !== 200) {
-                    // 不成功删掉token让其重新进入
-                    wx.removeStorageSync('token')
-                    wx.showModal({title: '提示', content:'session过期，请退出小程序重新进入', showCancel:false})
-                }
-                app.globalData.userInfo = res.data.data
-                self.setData({
-                    userInfo: res.data.data,
-                    isLogin: true
-                })
-            }
-        })
-    },
-    showEmoj() {
-    },
-    //调用图片拍照功能
-    showPhone() {
-      let that = this
-      wx.chooseImage({
-        count: 1, // 默认9 暂时支持一张
-        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function (res) {
-          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-          var tempFilePaths = res.tempFilePaths
-          // 将文件上传至阿里云
-          that.requestToken(tempFilePaths)
-        },
-        fail() {
-          // wx.showModal({
-          //   title: '提示',
-          //   content: '调用失败，请稍后重试',
-          //   success: function (res) {
-          //     if (res.confirm) {
-          //       console.log('用户点击确定')
-          //     } else if (res.cancel) {
-          //       console.log('用户点击取消')
-          //     }
-          //   }
-          // })
-        }
-      })
-    },
-    // 拿token
-    requestToken(tempFilePaths) {
-      let that = this
-      request({
-        url: '/api/wx/aliyun/form', 
-        method:'GET',
-        success: function (res) {
-          if (res.data.code =200) {
-            console.log(res.data.data)
-            that.uploadAliyun(res.data.data, tempFilePaths)
-          }
-        },
-      })
-    },
-    //上传阿里云
-    uploadAliyun(token, tempFilePaths) {
-      let that = this
-      let { endPoint,expire, ossAccessKeyId, policy, signature } = token
-      //去掉微信中路径
-      let filename = tempFilePaths[0].replace('wxfile://', '')
-      // 过期返回
-      if (expire*1000 < new Date().getTime()) {
-        wx.showToast({
-          title: '验证过期',
-          icon: 'success',
-          duration: 2000
-        })
-        return
+  data: {
+    animate: true,
+    placeholderText: "连接服务器中...",
+    messageArray: [
+      //   {
+      //     id:'0',
+      //     type: 'self',
+      //     name: 'wang',
+      //     time: '2000-2-2',
+      //     message: 'zhongjiashigedashabi'
+      // }
+    ],
+    socketOpen: false,
+    inputValue: "",
+    userInfo: {},
+    roomId: -1,
+    isLogin: false, //判断是否登录，显示/隐藏登录btn
+    focusHeight: '8px',
+    showGift: false, //显示礼物组件
+  },
+  onLoad: function (options) {
+    this.data.roomId = options.roomId || 1;
+    if (app.globalData.token) {
+      this.checkUserInfo()
+      this.initStompClient()
+    } else {
+      app.loginCallback = res => {
+        this.checkUserInfo()
+        this.initStompClient()
       }
-      wx.uploadFile({
-        url: endPoint, 
-        header: { 'content-type':'multipart/form-data'},
-        filePath: tempFilePaths[0], //暂时只支持上传一张
-        name: 'file',
-        formData: {
-          name: tempFilePaths[0],
-          key: filename ,
-          policy,
-          ossAccessKeyId,
-          success_action_status:'200',
-          signature,
-        },
-        success: function (res) {
-          console.log('上传阿里云成功')
-          // 在微信开发工具中filename改为下面
-          // filename = 'http://tmp/' + filename
-          that.sendSocketMessage({ type: 'IMAGE', detail: `${chatConfig.httpProtocol}${chatConfig.uploadHost}/${filename}` })
-        },
-        fail: function(res) {
-          console.log(res)
+    }
+  },
+  onUnload: function () {
+    // 关闭socket
+    wx.closeSocket();
+  },
+  bindKeyInput: function (e) {
+    this.setData({
+      inputValue: e.detail.value
+    });
+  },
+  // 连接websocket聊天 接受广播信息
+  initStompClient: function () {
+    const self = this;
+    client.init(() => {
+      this.setData({
+        socketOpen: true,
+        placeholderText: "连接服务器成功，请输入信息。"
+      })
+    });
+    client.connect({ 'wx-group-token': app.globalData.token }, function (sessionId) {
+      client.subscribe(chatConfig.subcribeUrl + '/' + self.data.roomId, function (body, headers) {
+        var data = JSON.parse(body.body).data;
+        console.log('接收');
+        console.log(data);
+        // this.handleMessage(data.message)
+        var newMessage = {
+          id: data.message.id,
+          type: 'other',
+          isText: data.message.type === 'TEXT' ? true : false,
+          name: data.user.nikename,
+          time: 'message.sendTime',
+          avatarUrl: data.user.avatarurl,
+          message: data.message.detail
+        };
+        newMessage.type = data.user.openid === self.data.userInfo.openId ? 'self' : 'other';
+        var newArray = [...self.data.messageArray, newMessage];
+        self.setData({
+          messageArray: newArray,
+          placeholderText: "请输入信息",
+          num: newArray[newArray.length - 1].id
+        });
+      });
+    })
+  },
+  send: function () {
+    if (this.data.inputValue !== "") {
+      this.sendSocketMessage({ type: 'TEXT', detail: this.data.inputValue });
+      this.setData({
+        inputValue: ""
+      });
+    }
+  },
+  // 发送websocket信息
+  // @param {
+  //    type: '',
+  //    detail:
+  // }
+  sendSocketMessage: function (obj) {
+    var self = this;
+    if (self.data.socketOpen) {
+      client.send(chatConfig.sendMsgUrl + '/' + self.data.roomId,
+        { priority: 9 },
+        JSON.stringify(obj))
+    }
+  },
+  checkUserInfo: function (e) {
+    wx.getUserInfo({
+      // 带上登录信息
+      withCredentials: true,
+      success: res => {
+        // 成功获取用户信息
+        this.sendEncryptedData(res.encryptedData, res.iv)
+      },
+      fail: res => {
+        console.log(res)
+        // 显示登录按钮，引导登录
+        this.setData({
+          isLogin: false,
+        })
+      }
+    })
+  },
+  // 手动授权登录
+  updateUserInfo: function (res) {
+    console.log(res)
+    this.sendEncryptedData(res.detail.encryptedData, res.detail.iv)
+  },
+  // 获取用户信息
+  sendEncryptedData: function (encryptedData, iv) {
+    let self = this
+    request({
+      url: '/api/wx/user',
+      data: {
+        encryptedData: encryptedData,
+        iv: iv
+      },
+      success: function (res) {
+        console.log(res)
+        if (res.data.code !== 200) {
+          // 不成功删掉token让其重新进入
+          wx.removeStorageSync('token')
+          wx.showModal({ title: '提示', content: 'session过期，请退出小程序重新进入', showCancel: false })
         }
+        app.globalData.userInfo = res.data.data
+        self.setData({
+          userInfo: res.data.data,
+          isLogin: true
+        })
+      }
+    })
+  },
+  showEmoj() {
+  },
+  //调用图片拍照功能
+  showPhone() {
+    let that = this
+    wx.chooseImage({
+      count: 1, // 默认9 暂时支持一张
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        // 将文件上传至阿里云
+        that.requestToken(tempFilePaths)
+      },
+      fail() {
+      }
+    })
+  },
+  // 拿token
+  requestToken(tempFilePaths) {
+    let that = this
+    request({
+      url: '/api/wx/aliyun/form',
+      method: 'GET',
+      success: function (res) {
+        if (res.data.code = 200) {
+          console.log(res.data.data)
+          that.uploadAliyun(res.data.data, tempFilePaths)
+        }
+      },
+    })
+  },
+  //上传阿里云
+  uploadAliyun(token, tempFilePaths) {
+    let that = this
+    let { endPoint, expire, ossAccessKeyId, policy, signature } = token
+    //去掉微信中路径
+    let filename = tempFilePaths[0].replace('wxfile://', '')
+    // 过期返回
+    if (expire * 1000 < new Date().getTime()) {
+      wx.showToast({
+        title: '验证过期',
+        icon: 'success',
+        duration: 2000
       })
-    },
-    focus(e) {
-      // this.setData({
-      //   focusHeight: '10px'
-      // })
-    },
-    // 监听子组件close 关闭gift-cont
-    close(val) {
-      console.log(val)
-      this.setData({
-        showGift: false
-      })
-    },
-    showGiftComponent() {
-      this.setData({
-        showGift: true
-      })
-    },
-    // toggleDialog(show) {
-    //   var animationClass = show ? "slipUp" : "slipBottom";
-    //   var animation = function () {
-    //     var ele = document.getElementById("dialog-face");
-    //     ele.className = "dialog-face " + animationClass;
-    //     ele = document.getElementById("dialog");
-    //     ele.className = "dialog-root " + animationClass;
-    //     ele = document.getElementById("dialog-wrapper");
-    //     ele.className = "dialog-wrapper " + animationClass;
-    //   };
-    //   setTimeout(animation, 100);
-    // }
+      return
+    }
+    wx.uploadFile({
+      url: endPoint,
+      header: { 'content-type': 'multipart/form-data' },
+      filePath: tempFilePaths[0], //暂时只支持上传一张
+      name: 'file',
+      formData: {
+        name: tempFilePaths[0],
+        key: filename,
+        policy,
+        ossAccessKeyId,
+        success_action_status: '200',
+        signature,
+      },
+      success: function (res) {
+        console.log('上传阿里云成功')
+        // 在微信开发工具中filename改为下面
+        // filename = 'http://tmp/' + filename
+        that.sendSocketMessage({ type: 'IMAGE', detail: `${chatConfig.httpProtocol}${chatConfig.uploadHost}/${filename}` })
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  focus(e) {
+    // this.setData({
+    //   focusHeight: '10px'
+    // })
+  },
+  // 监听子组件close 关闭gift-cont
+  close(val) {
+    console.log(val)
+    this.setData({
+      showGift: false
+    })
+  },
+  showGiftComponent() {
+    this.setData({
+      showGift: true
+    })
+    // var alert = document.getElementById('box');
+    // requestAnimationFrame(function () {
+    //   box.setAttribute('class', 'box mov');
+    // })
+  },
+  /*
+  *监听子组件sendGift事件，触发发送礼物
+  */
+  sendGift(val) {
+    this.sendSocketMessage({ type: 'GIFT', detail: val.detail.detail });
+  }
 });
