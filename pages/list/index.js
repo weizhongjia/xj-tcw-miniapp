b326326c83d6678d0e7c341133dc45f570319356
weizhongjia <948913886@qq.com>
2018-07-01 22:50:08 +0800

    首页接口

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,6 +1,6 @@
 //获取应用实例
 const app = getApp()
-
+const {request} = require('../../utils/request')
 Page({
   /**
    * 页面的初始数据
@@ -32,7 +32,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    if (app.globalData.token) {
+      this.checkUserInfo()
+    } else {
+      app.loginCallback = res => {
+        this.checkUserInfo()
+      }
+    }
     this.setData({
       // 并没有像vue一样做代理所以不能this.MovieList调用
       currentIndex: this.data.MovieList.length - 1
@@ -86,7 +92,73 @@ Page({
    */
   onShareAppMessage: function () {
     console.log("转发")
-  }
-
-
+  },
+  getHistoryVideo: function () {
+    const self = this;
+    request({
+      url: '/api/wx/video',
+      data: {
+        orderId: 100000,
+        size: 10
+      },
+      method: "GET",
+      success: function(res) {
+        console.log(res)
+        self.setData({
+          MovieList: res.data.data
+        })
+      }
+    })
+  },
+  checkUserInfo: function(e) {
+    wx.getUserInfo({
+      // 带上登录信息
+      withCredentials: true,
+      success: res => {
+        // 成功获取用户信息
+        this.sendEncryptedData(res.encryptedData, res.iv)
+        this.getHistoryVideo()
+      },
+      fail: res => {
+        console.log(res)
+        // 显示登录按钮，引导登录
+        this.setData({
+          isLogin: false,
+        })
+      }
+    })
+  },
+// 手动授权登录
+  updateUserInfo: function(res) {
+    console.log(res)
+    this.sendEncryptedData(res.detail.encryptedData, res.detail.iv)
+  },
+  // 获取用户信息
+  sendEncryptedData: function(encryptedData, iv) {
+    let self = this
+    request({
+      url: '/api/wx/user',
+      data: {
+        encryptedData: encryptedData,
+        iv: iv
+      },
+      success: function(res) {
+        console.log(res)
+        if (res.data.code !== 200) {
+          // 不成功删掉token让其重新进入
+          wx.removeStorageSync('token')
+          wx.showModal({
+            title: '提示',
+            content: 'session过期，请退出小程序重新进入',
+            showCancel: false
+          })
+        }
+        app.globalData.userInfo = res.data.data
+        self.setData({
+          userInfo: res.data.data,
+          isLogin: true
+        })
+      }
+    })
+  },
 })
