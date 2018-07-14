31ad8572f8a2acd0a2f60af5386ddd7814555f9f
jiangshicun007 <jsc@yuancore.com>
2018-07-14 16:13:09 +0800

    please wait for us

--- components/mine/index.js
+++ components/mine/index.js
@@ -35,7 +35,19 @@ Component({
     })
   },
   methods: {
-
+    wait() {
+      wx.showModal({
+        title: '',
+        content: '敬请期待!',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
   },
 
 })
